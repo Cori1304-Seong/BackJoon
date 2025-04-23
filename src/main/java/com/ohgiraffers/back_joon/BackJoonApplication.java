6b42273904398153611552a90f8f3f2b1ef9772c
Cori1304-Seong <andyjm7739@gmail.com>
2025-04-23 15:52:12 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/back_joon/BackJoonApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.back_joon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackJoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackJoonApplication.class, args);
+    }
+
+}
